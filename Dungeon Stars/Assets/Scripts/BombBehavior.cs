0767c28e1cee213c5be2738bf183f85a6ad39719
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-10-04 22:31:37 -0400

    Organized & Added new bombs

--- Dungeon Stars/Assets/Scripts/BombBehavior.cs
+++ Dungeon Stars/Assets/Scripts/BombBehavior.cs
@@ -37,7 +37,7 @@ public class BombBehavior : ProjectileBehavior
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(proxy && other.tag == "Enemy")
+        if(proxy && other.tag == "Obstacle")
         {
             Detonate();
         }
