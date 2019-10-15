036fc7a21c0f3de0db9f7d440232f492e7d576eb
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-10-14 20:59:46 -0400

    Working Main Menu!
    Thanks Brackeys!

---
+++ Dungeon Stars/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneToLoad; 
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void Play2Player()
+    {
+        print("I dont have any friends to play with so these is no multiplayer yet D:");
+    }
+
+    public void QuitGame()
+    {
+        print("*Windows shut down sound effect*");
+        Application.Quit();
+    }
+
+}
