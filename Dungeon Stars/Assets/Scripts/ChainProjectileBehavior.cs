2ea07430a2ba6df66ca043f7433793b58dc57315
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-11-02 14:45:29 -0400

    Cool Lightning

--- Dungeon Stars/Assets/Scripts/ChainProjectileBehavior.cs
+++ Dungeon Stars/Assets/Scripts/ChainProjectileBehavior.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class ChainProjectileBehavior : BombBehavior
 {
 
-    //[Header("Chain Behavior")]
+    [Header("Chain Behavior")]
     //public float dpf;  // Damage per frame/update loop 
 
-    private LineRenderer lineRenderer;
+    //private LineRenderer lineRenderer;
+    public GameObject lightning;
+    public Transform lightningEnd;
 
     // Start is called before the first frame update
     void Start()
     {
         base.Start();
-        lineRenderer = GetComponent<LineRenderer>();
+        lightning.SetActive(false);
+        //lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void FixedUpdate()
@@ -32,13 +35,25 @@ public class ChainProjectileBehavior : BombBehavior
     {
         if (other.tag == "Obstacle")
         {
+            lightning.SetActive(true);
             ObstacleBehavior entity = other.gameObject.GetComponent<ObstacleBehavior>();
             //Chain lightning
 
-            Vector3[] chain = { transform.position, entity.gameObject.transform.position };
+            lightningEnd.position = entity.gameObject.transform.position;
 
-            lineRenderer.SetPositions(chain);
+            //Vector3[] chain = { transform.position, entity.gameObject.transform.position };
+
+
+            //lineRenderer.SetPositions(chain);
             entity.hp -= dmgValue;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            lightning.SetActive(false);
         }
     }
 }
