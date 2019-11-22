a54e98f3dd64132d9320cfe7898b58321a52e78f
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-11-22 17:35:19 -0500

    OmniController Stuff

--- Dungeon Stars/Assets/Scripts/OmniController.cs
+++ Dungeon Stars/Assets/Scripts/OmniController.cs
@@ -7,11 +7,19 @@ public class OmniController : MonoBehaviour
 {
     public static OmniController omniController;
 
+    [Header("Game Summary")]
     public int totalScore;
     public int enemiesKilled;
     public int timesDied;
     public int powerUpsCollected;
 
+    [Header("Options")]
+    public GameObject[] allShips;
+    public bool enableAllShips;
+    public bool enableDebug;
+    public bool infiniteLives;
+
+    [Header("Two Player Co-op")]
     public bool twoPlayerMode;
 
     private void Awake()
@@ -27,4 +35,11 @@ public class OmniController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
     
+    public void ResetGameplayVariables()
+    {
+        totalScore = 0;
+        enemiesKilled = 0;
+        timesDied = 0;
+        powerUpsCollected = 0;
+    }
 }
