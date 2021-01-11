301479e731a3e1845e508da060fb802222b63079
Andrew-64 <aa6th@virginia.edu>
2021-01-10 23:38:59 -0500

    Explosion Lighting

---
+++ Dungeon Stars/Assets/Scripts/ExplodeIllumination.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class ExplodeIllumination : MonoBehaviour
+{
+    public float startIntensity;
+    public float endIntensity;
+    public float duration;
+    float startingTime;
+    float currIntensity = 0;
+
+    Light2D illumination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        illumination = GetComponent<Light2D>();
+        currIntensity = startIntensity;
+        startingTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        illumination.intensity = Mathf.Max(0f,
+            startIntensity - ((Time.time - startingTime) / duration) * startIntensity
+            );
+    }
+}
