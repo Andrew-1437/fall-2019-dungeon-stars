b808a4d6a2592af4ebf9abc3af4fe2db13c6f848
Andrew-64 <aa6th@virginia.edu>
2021-07-06 20:10:58 -0400

    Displaying Rank On Game Summary Scene Code

--- Dungeon Stars/Assets/Scripts/Summary.cs
+++ Dungeon Stars/Assets/Scripts/Summary.cs
@@ -16,6 +16,8 @@ public class Summary : MonoBehaviour
     string bestDeathsStr;
     string highScoreStr;
 
+    public int rank = 0;
+
     int trueScore = 0;  // True high score, after applying all modifiers
     float scoreModifier = 1f;
 
@@ -61,7 +63,8 @@ public class Summary : MonoBehaviour
         else
             highScoreStr = "   Best: " + highScore.ToString();
 
-
+        GetRanking();
+        // Show animation of rank appearing
         
     }
 
@@ -98,5 +101,79 @@ public class Summary : MonoBehaviour
         // If playing multiplayer, apply x1.1 to score
         if (OmniController.omniController.twoPlayerMode)
             scoreModifier *= 1.1f;
+    }
+
+    // Calculate the rank from 0-6 (F, D, C, B, A, S Tier)
+    public void GetRanking()
+    {
+        /* ***********************
+         * Criteria for S rank
+         * Enemies Killed: >= 550
+         * Power Ups Collected >= 45
+         * Deaths <= 0
+         * Final Score >= 1,500,000
+         * 
+         * ***********************
+         * Criteria for A Rank
+         * Enemies Killed: >= 500
+         * Power Ups Collected >= 40
+         * Deaths <= 1
+         * Final Score >= 1,300,000
+         * 
+         * ***********************
+         * Criteria for B rank
+         * Enemies Killed: >= 450
+         * Power Ups Collected >= 35
+         * Deaths <= 3
+         * Final Score >= 1,000,000
+         * 
+         * ***********************
+         * Criteria for C rank
+         * Enemies Killed: >= 370
+         * Power Ups Collected >= 30
+         * Deaths <= 4
+         * Final Score >= 800,000
+         * 
+         * ***********************
+         * Criteria for D rank
+         * Enemies Killed: >= 300
+         * Power Ups Collected >= 20
+         * Deaths <= 5
+         * Final Score >= 500,000
+         */
+
+        if(OmniController.omniController != null)
+        {
+            // D Rank
+            if (OmniController.omniController.enemiesKilled >= 300 &&
+                OmniController.omniController.powerUpsCollected >= 20 &&
+                OmniController.omniController.timesDied <= 5 &&
+                trueScore >= 500000)
+                rank++;
+            // C Rank
+            if (OmniController.omniController.enemiesKilled >= 370 &&
+                OmniController.omniController.powerUpsCollected >= 30 &&
+                OmniController.omniController.timesDied <= 4 &&
+                trueScore >= 800000)
+                rank++;
+            // B Rank
+            if (OmniController.omniController.enemiesKilled >= 450 &&
+                OmniController.omniController.powerUpsCollected >= 35 &&
+                OmniController.omniController.timesDied <= 3 &&
+                trueScore >= 1000000)
+                rank++;
+            // A Rank
+            if (OmniController.omniController.enemiesKilled >= 500 &&
+                OmniController.omniController.powerUpsCollected >= 40 &&
+                OmniController.omniController.timesDied <= 1 &&
+                trueScore >= 1300000)
+                rank++;
+            // S Rank
+            if (OmniController.omniController.enemiesKilled >= 550 &&
+                OmniController.omniController.powerUpsCollected >= 45 &&
+                OmniController.omniController.timesDied <= 0 &&
+                trueScore >= 1500000)
+                rank++;
+        }
     }
 }
