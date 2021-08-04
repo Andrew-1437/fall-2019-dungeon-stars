289ca70400efee2ad1582e1b322996efff77ccc0
Andrew-64 <aa6th@virginia.edu>
2021-08-04 17:24:21 -0400

    Boss 1 Rework - Have boss1 behavior script

--- Dungeon Stars/Assets/Scripts/Boss1Behavior.cs
+++ Dungeon Stars/Assets/Scripts/Boss1Behavior.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
-public class Boss1Movement : MonoBehaviour {
+public class Boss1Behavior : MonoBehaviour {
 
+    [Header("Begining Movement Variables")]
 	public float moveYToHere;
     public float rotateTo;
     public float moveSpeed;
     public float rotateSpeed;
 
+    [Header("References")]
     public GameObject bossFlowchart;
+    public GameObject weakSpot;
+
+    BossBehavior boss;
+
+    bool stage2 = false;
+
+    private void Start()
+    {
+        boss = GetComponent<BossBehavior>();
+    }
 
     // Wow who wrote this. Oh wait it was me
     private void Update()
     {
+        // Begining Movement (Old code)
         if (GetComponent<BossBehavior>().awake)
         {
             if (transform.position.y < moveYToHere)
@@ -36,6 +50,19 @@ public class Boss1Movement : MonoBehaviour {
                 GetComponent<BossBehavior>().activeAllTriggers(true);
             }
         }
+
+        // If all four turrets are destroyed, go to stage 2 (reveal fifth weak point and change moveset)
+        if (boss.turrets <= 1 && !stage2)
+        {
+            stage2 = true;
+            bossFlowchart.GetComponent<Fungus.Flowchart>().SendFungusMessage("stage2");
+        }
+    }
+
+    public void RevealWeakSpot(bool active)
+    {
+        weakSpot.GetComponent<Collider2D>().enabled = active;
+        weakSpot.GetComponent<Light2D>().enabled = active;
     }
 
 }
