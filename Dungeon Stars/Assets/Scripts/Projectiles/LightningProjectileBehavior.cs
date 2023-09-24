3448800ccec7f29d334f85740c6a9193991302f0
Andrew-64 <aa6th@virginia.edu>
2023-09-24 12:08:16 -0500

    Cool new lightning weapon

---
+++ Dungeon Stars/Assets/Scripts/Projectiles/LightningProjectileBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class LightningProjectileBehavior : ProjectileBehavior
+{
+    public float MaxAnglePerFrame;
+
+    protected void FixedUpdate()
+    {
+        if (MaxAnglePerFrame > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f,
+                transform.rotation.eulerAngles.z + Random.Range(-MaxAnglePerFrame, MaxAnglePerFrame));
+        }
+
+        base.FixedUpdate();
+    }
+    
+}
